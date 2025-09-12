167785c21e39e32de50df59dbfa653ba73b0a4ac
Scott McHenry <smchenry2014@gmail.com>
2025-09-12 16:12:13 -0400

    Syncing the outputs between the two terraform configurations as much as possible

--- aws/outputs.tf
+++ aws/outputs.tf
@@ -1,11 +1,18 @@
+# Container Registry Outputs (equivalent to ACR)
 output "ecr_repository_url" {
   description = "The URL of the ECR repository"
   value       = aws_ecr_repository.filmdatahub.repository_url
 }
 
+# Kubernetes Cluster Outputs (equivalent to AKS)
 output "eks_cluster_endpoint" {
   description = "Endpoint for EKS control plane"
   value       = aws_eks_cluster.filmdatahub.endpoint
+}
+
+output "eks_cluster_name" {
+  description = "The name of the EKS cluster"
+  value       = aws_eks_cluster.filmdatahub.name
 }
 
 output "eks_cluster_security_group_id" {
@@ -23,6 +30,13 @@ output "eks_node_group_iam_role_name" {
   value       = aws_iam_role.eks_node_group.name
 }
 
+output "eks_kube_config_raw" {
+  description = "Raw Kubernetes config to be used by kubectl and other compatible tools"
+  value       = aws_eks_cluster.filmdatahub.certificate_authority[0].data
+  sensitive   = true
+}
+
+# Database Outputs (equivalent to MySQL Flexible Server)
 output "rds_endpoint" {
   description = "RDS instance endpoint"
   value       = aws_db_instance.filmdatahub.endpoint
@@ -36,5 +50,11 @@ output "rds_port" {
 output "rds_database_name" {
   description = "RDS database name"
   value       = aws_db_instance.filmdatahub.db_name
+}
+
+output "rds_administrator_login" {
+  description = "RDS instance administrator login"
+  value       = aws_db_instance.filmdatahub.username
+  sensitive   = true
 }
 
--- azure/outputs.tf
+++ azure/outputs.tf
@@ -1,28 +1,25 @@
-output "resource_group_name" {
-  description = "The name of the resource group"
-  value       = azurerm_resource_group.filmdatahub.name
-}
-
-output "resource_group_location" {
-  description = "The location of the resource group"
-  value       = azurerm_resource_group.filmdatahub.location
-}
-
-output "container_registry_login_server" {
-  description = "The login server URL of the container registry"
+# Container Registry Outputs (equivalent to ECR)
+output "acr_repository_url" {
+  description = "The URL of the Azure Container Registry"
   value       = azurerm_container_registry.filmdatahub.login_server
 }
 
-output "container_registry_admin_username" {
-  description = "The admin username for the container registry"
+output "acr_admin_username" {
+  description = "The admin username for the Azure Container Registry"
   value       = azurerm_container_registry.filmdatahub.admin_username
   sensitive   = true
 }
 
-output "container_registry_admin_password" {
-  description = "The admin password for the container registry"
+output "acr_admin_password" {
+  description = "The admin password for the Azure Container Registry"
   value       = azurerm_container_registry.filmdatahub.admin_password
   sensitive   = true
+}
+
+# Kubernetes Cluster Outputs (equivalent to EKS)
+output "aks_cluster_endpoint" {
+  description = "Endpoint for AKS control plane"
+  value       = azurerm_kubernetes_cluster.filmdatahub.fqdn
 }
 
 output "aks_cluster_name" {
@@ -30,100 +27,40 @@ output "aks_cluster_name" {
   value       = azurerm_kubernetes_cluster.filmdatahub.name
 }
 
-output "aks_cluster_fqdn" {
-  description = "The FQDN of the Azure Kubernetes Managed Cluster"
-  value       = azurerm_kubernetes_cluster.filmdatahub.fqdn
+output "aks_cluster_identity_principal_id" {
+  description = "The Principal ID associated with the AKS cluster managed identity"
+  value       = azurerm_kubernetes_cluster.filmdatahub.identity[0].principal_id
 }
 
-output "aks_cluster_private_fqdn" {
-  description = "The private FQDN of the Azure Kubernetes Managed Cluster"
-  value       = azurerm_kubernetes_cluster.filmdatahub.private_fqdn
+output "aks_node_pool_identity_object_id" {
+  description = "The Object ID of the user assigned identity for kubelet"
+  value       = azurerm_kubernetes_cluster.filmdatahub.kubelet_identity[0].object_id
 }
 
-output "aks_cluster_portal_fqdn" {
-  description = "The FQDN for the Azure Portal resources when private link is enabled"
-  value       = azurerm_kubernetes_cluster.filmdatahub.portal_fqdn
-}
-
-output "aks_cluster_kube_config_raw" {
+output "aks_kube_config_raw" {
   description = "Raw Kubernetes config to be used by kubectl and other compatible tools"
   value       = azurerm_kubernetes_cluster.filmdatahub.kube_config_raw
   sensitive   = true
 }
 
-output "aks_cluster_kube_config_host" {
-  description = "The Kubernetes cluster server host"
-  value       = azurerm_kubernetes_cluster.filmdatahub.kube_config.0.host
-  sensitive   = true
-}
-
-output "aks_cluster_identity_principal_id" {
-  description = "The Principal ID associated with this Managed Service Identity"
-  value       = azurerm_kubernetes_cluster.filmdatahub.identity[0].principal_id
-}
-
-output "aks_cluster_kubelet_identity_object_id" {
-  description = "The Object ID of the user assigned identity for kubelet"
-  value       = azurerm_kubernetes_cluster.filmdatahub.kubelet_identity[0].object_id
-}
-
-output "mysql_server_name" {
-  description = "The name of the MySQL Flexible Server"
-  value       = azurerm_mysql_flexible_server.filmdatahub.name
-}
-
-output "mysql_server_fqdn" {
-  description = "The FQDN of the MySQL Flexible Server"
+# Database Outputs (equivalent to RDS)
+output "mysql_endpoint" {
+  description = "MySQL Flexible Server endpoint"
   value       = azurerm_mysql_flexible_server.filmdatahub.fqdn
 }
 
-output "mysql_server_administrator_login" {
-  description = "The administrator login name for the MySQL Flexible Server"
-  value       = azurerm_mysql_flexible_server.filmdatahub.administrator_login
-  sensitive   = true
+output "mysql_port" {
+  description = "MySQL Flexible Server port"
+  value       = 3306
 }
 
 output "mysql_database_name" {
-  description = "The name of the MySQL database"
+  description = "MySQL database name"
   value       = azurerm_mysql_flexible_database.filmdatahub.name
 }
 
-output "mysql_database_charset" {
-  description = "The charset of the MySQL database"
-  value       = azurerm_mysql_flexible_database.filmdatahub.charset
-}
-
-output "mysql_database_collation" {
-  description = "The collation of the MySQL database"
-  value       = azurerm_mysql_flexible_database.filmdatahub.collation
-}
-
-output "mysql_firewall_rule_name" {
-  description = "The name of the MySQL firewall rule"
-  value       = azurerm_mysql_flexible_server_firewall_rule.allow_aks_only.name
-}
-
-output "mysql_firewall_rule_start_ip" {
-  description = "The starting IP address of the MySQL firewall rule"
-  value       = azurerm_mysql_flexible_server_firewall_rule.allow_aks_only.start_ip_address
-}
-
-output "mysql_firewall_rule_end_ip" {
-  description = "The ending IP address of the MySQL firewall rule"
-  value       = azurerm_mysql_flexible_server_firewall_rule.allow_aks_only.end_ip_address
-}
-
-output "acr_pull_role_assignment_id" {
-  description = "The ID of the role assignment for ACR pull access"
-  value       = azurerm_role_assignment.filmdatahub.id
-}
-
-output "acr_pull_principal_id" {
-  description = "The principal ID for the ACR pull role assignment"
-  value       = azurerm_role_assignment.filmdatahub.principal_id
-}
-
-output "acr_pull_role_definition_name" {
-  description = "The role definition name for the ACR pull role assignment"
-  value       = azurerm_role_assignment.filmdatahub.role_definition_name
+output "mysql_administrator_login" {
+  description = "MySQL Flexible Server administrator login"
+  value       = azurerm_mysql_flexible_server.filmdatahub.administrator_login
+  sensitive   = true
 }
