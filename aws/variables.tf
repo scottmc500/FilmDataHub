7464c25ffbb1c6456b7c68dd305115b044a76eca
Scott McHenry <smchenry2014@gmail.com>
2025-09-12 13:35:44 -0400

    Built the entire AWS architecture

---
+++ aws/variables.tf
@@ -0,0 +1,12 @@
+variable "db_admin_username" {
+  description = "The login username for the MySQL Database"
+  type        = string
+  sensitive   = true
+}
+
+variable "db_admin_password" {
+  description = "The login password for the MySQL Database"
+  type        = string
+  sensitive   = true
+}
+
